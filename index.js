fc3ae38eed8f8cf81db1ab245447d51d61e15817
Abdulqasem-Bakhshi <q.bakhshi@outlook.com>
2025-04-05 10:02:50 +0200

    update with index.html + index.css files

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const {app, BrowserWindow} = require('electron');
+const electron = require('electron');
+const BrowserWindow = require('electron').BrowserWindow;
+const app = require('electron').app;
+const path = require('path');
 
-app.on('ready', () => {
+// Function to create the main window of the application
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
-    frame: true,
-  })
-})
+    frame: true
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+
+app.on("ready", createWindow);
